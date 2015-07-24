ca5fdbcab2d420fffa93554db5dd3e94087ccfa1
therohboat <william.roh@stonybrook.edu>
2015-07-24 15:01:30 -0400

    Adding some inputs and outputs

--- basic.cpp
+++ basic.cpp
@@ -1,13 +1,17 @@
 //First thing written in Carlos' C++ tutorial 
 
+#include <iostream> //Include libraries which include standard things like cout, endl (you can see std::endl or std::cout)
+
 int main()
 {
   int a=1;
+  std::cout << "a=" << a << std::endl;
   if(a==1){
     a=2;
   }else
     {
       //This part never executes
     }
+  std::cout << "a=" << a << std::endl;
   return 0;
 }
