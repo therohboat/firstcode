e5aa03a25089e61a7126c76f0bb7192c34fd99fc
therohboat <william.roh@stonybrook.edu>
2015-07-31 14:56:32 -0400

    Revision 2

--- 02_string/basic.cpp
+++ 02_string/basic.cpp
@@ -5,6 +5,19 @@ int main(){
 	   std::cout << "Enter your name:" ;
 	   std::string sName; //String is part of the standard library, it is NOT part of C++ core langauge
 	   std::cin >> sName;
-	   std::cout << "Hello, " << sName << ". Nice to meet you." << std::endl;
+
+	   std::string sMessage = "Hello, " + sName + ". Nice to meet you.";
+	   std::string sBlank( sMessage.size(), ' ' );
+	   std::string sLine2( "|| " + sBlank + " ||");
+	   std::string sLine1( sLine2.size(), '=' );
+
+	   std::cout << std::endl;
+	   std::cout << sLine1 << std::endl;
+	   std::cout << sLine2 << std::endl;
+	   std::cout << "|| " << sMessage << " ||" <<std::endl;
+	   std::cout << sLine2 << std::endl;
+	   std::cout << sLine1 << std::endl;
+	   std::cout << std::endl;
+
 	   return 0;
 	   }
